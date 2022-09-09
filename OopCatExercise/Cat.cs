9a0aa422b21dde11233d7120f95792b441b335f7
VKamble61 <varsha.kamble61@gmail.com>
2022-09-09 22:54:41 +0100

    Finished program All testcases Passed

--- OopCatExercise/Cat.cs
+++ OopCatExercise/Cat.cs
@@ -1,8 +1,79 @@
 ﻿using System;
+//using System.Diagnostics.CodeAnalysis;
+using OopCatExercise;
+
 namespace OopCatExercise
 {
-	public abstract class Cat : ICat
-	{
+    public abstract class Cat : ICat
+    {
+        public string Setting { get; set; }
+        public bool IsAsleep { get; set; }
+        public int AverageHeight { get; set; }
 
-	}
+        public Cat()
+        {
+           
+        }
+        public abstract string Eat();
+
+        public virtual void WakeUp() { }
+
+        public virtual void GoToSleep() { }       
+
+
+    }
+
+    public class LionCat : Cat
+    {
+        public LionCat() : base()
+        {
+            AverageHeight = 1100;
+        }
+
+
+        public override string Eat()
+        {
+            return "Roar!!!!";
+        }
+        
+
+    }
+    public class CheetahCat : Cat
+    {
+        public CheetahCat() : base()
+        {
+            AverageHeight = 1100;
+        }
+        public override string Eat()
+        {
+            return "Zzzzzzz";
+        }      
+
+
+    }
+    public class DomesticCat : Cat
+    {
+        public DomesticCat() : base()
+        {
+            IsAsleep = false;
+            Setting = "domestic";
+            AverageHeight = 23;
+        }
+
+        public override string Eat()
+        {
+            return "Purrrrrrr";
+        }
+        public override void GoToSleep()
+        {
+            IsAsleep = true;
+        }
+
+        public override void WakeUp()
+        {
+            IsAsleep = false;
+        }
+
+    }
+
 }
--- OopCatExercise/ICat.cs
+++ OopCatExercise/ICat.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿//using System;
 namespace OopCatExercise
 {
 	public interface ICat
 	{
-	}
+        public void GoToSleep();
+        public void WakeUp();
+        public string Eat();       
+
+    }
 }
